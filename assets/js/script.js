cfdcaf93879908f3e3dae7917c180ef44b085878
Norrland <79220667+norrland90@users.noreply.github.com>
2023-06-29 20:59:02 +0200

    add font selector javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,18 @@ function toggleMode() {
 
 modeSwitch.addEventListener('click', toggleMode);
 
-// Show card with fonts
+// Show card with fonts and change fonts
 const showFontsArrow = document.querySelector('.header__show-fonts-btn');
+const fontCard = document.querySelector('.header__font-card');
 
 // Rotate arrow + show font card on click
 function onArrowClick() {
   const fontArrow = document.querySelector('.header__arrow-icon');
   fontArrow.classList.toggle('rotate');
+  toggleFontCard();
+}
 
-  const fontCard = document.querySelector('.header__font-card');
+function toggleFontCard() {
   if (fontCard.classList.contains('hidden')) {
     fontCard.classList.remove('hidden');
     fontCard.style.transform = 'translateY(0)';
@@ -27,4 +30,38 @@ function onArrowClick() {
   }
 }
 
+function changeFont(e) {
+  if (e.target.id === 'sans-btn') {
+    changeCurrentFont('sans');
+  } else if (e.target.id === 'serif-btn') {
+    changeCurrentFont('serif');
+  } else if (e.target.id === 'mono-btn') {
+    changeCurrentFont('mono');
+  }
+}
+
+function changeCurrentFont(font) {
+  const currentFont = document.querySelector('.header__current-font');
+  if (font === 'sans') {
+    document.body.style.fontFamily = 'var(--ft-family-sans)';
+    currentFont.innerText = 'Sans Serif';
+  } else if (font === 'serif') {
+    document.body.style.fontFamily = 'var(--ft-family-serif)';
+    currentFont.innerText = 'Serif';
+  } else {
+    document.body.style.fontFamily = 'var(--ft-family-mono)';
+    currentFont.innerText = 'Mono';
+  }
+}
+
+// Vill gärna få att funka om man trycker utanför kortet - ej som nu när man trycker utanför font-selector-diven
+function hideFontCardOnClickOutside(e) {
+  let element = e.target.closest('.header__font-selector');
+  if (!element && !fontCard.classList.contains('hidden')) {
+    onArrowClick();
+  }
+}
+
 showFontsArrow.addEventListener('click', onArrowClick);
+fontCard.addEventListener('click', changeFont);
+document.addEventListener('click', hideFontCardOnClickOutside);
